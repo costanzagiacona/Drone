bd56a7d347694d4e1585de55d5d055596807e4b6
IlariaCosta <106031483+IlariaCosta@users.noreply.github.com>
2025-02-07 18:02:21 +0100

    Add files via upload

---
+++ prova.m
@@ -0,0 +1,29 @@
+ %Parametri del sistema
+    g = 9.81;  % accelerazione gravitazionale (m/s^2)
+    m = 0.45;     % massa (kg)
+    Izz = 8e-3;    % kg*m^2
+    p = 1;
+    k = p/Izz;     % costante di guadagno
+    
+    % Matrice A e B del sistema
+    A = [0,1,  0,  0;
+         0,0, -g/m, 0;
+         0,0, 0, 1;
+         0,0, 0, 0];
+    
+    B = [0; 0; 0; k];
+
+    % Posizioni desiderate degli autovalori (ad esempio, -1, -2, -3)
+    desired_poles = [-4,-1, -2, -3];
+    
+    % Calcola il guadagno K che sposta gli autovalori desiderati
+    %K = place(A, B, desired_poles)
+    % Calcolo del guadagno K con il metodo di Ackermann
+    K = acker(A, B, desired_poles);
+
+    % Matriss modificata A - BK
+    AA = A - B * K
+    % Verifica che gli autovalori di A_mod abbiano parte reale negativa
+    eig(AA)
+
+   
